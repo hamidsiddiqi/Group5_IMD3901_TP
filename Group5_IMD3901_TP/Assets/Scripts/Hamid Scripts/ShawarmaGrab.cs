8335f23b1d6f18b37ef0fdd2ec046c5b0075e9a6
hamidsidd7 <hamidsiddiqi458@gmail.com>
2026-02-12 18:39:31 -0500

    Adding in materials and adjusting script for ShawarmaGrab

--- Group5_IMD3901_TP/Assets/Scripts/Hamid Scripts/ShawarmaGrab.cs
+++ Group5_IMD3901_TP/Assets/Scripts/Hamid Scripts/ShawarmaGrab.cs
@@ -1,16 +1,35 @@
 using UnityEngine;
+using System.Collections; // Required for Coroutines
 
 public class ShawarmaGrab : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    // Drag your objects from the Hierarchy into these slots
+    public GameObject shawarmaFlat;
+    public GameObject shawarmaReady;
+
+    public float prepareTime = 12f; // Easy to change in the Inspector
+
     void Start()
     {
-        
+        // Set the initial state
+        if (shawarmaFlat != null) shawarmaFlat.SetActive(true);
+        if (shawarmaReady != null) shawarmaReady.SetActive(false);
+
+        // Start the countdown
+        StartCoroutine(PreparationTimer());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator PreparationTimer()
     {
-        
+        Debug.Log("Cooking started...");
+
+        // Wait for the specified seconds
+        yield return new WaitForSeconds(prepareTime);
+
+        // Swap the objects
+        if (shawarmaFlat != null) shawarmaFlat.SetActive(false);
+        if (shawarmaReady != null) shawarmaReady.SetActive(true);
+
+        Debug.Log("Shawarma is ready!");
     }
 }
