897a6a22f802d73ae8af99dbe285c362e1feeb60
garnkai <garn0092@algonquinlive.com>
2026-03-29 20:59:34 -0400

    customers moving nicely

--- Group5_IMD3901_TP/Assets/Scripts/CustomerMovement.cs
+++ Group5_IMD3901_TP/Assets/Scripts/CustomerMovement.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class CustomerMovement : MonoBehaviour
 {
 
-    public Transform startPos;
-    public Transform orderPos;
-    public Transform turnPos;
-    public Transform pickUpPos;
-    public Transform leavePos;
+    public GameObject startPos;
+    public GameObject orderPos;
+    public GameObject turnPos;
+    public GameObject pickUpPos;
+    public GameObject leavePos;
 
     public int orderNum;
-    private Transform[] moves;
+    private GameObject[] moves;
     public int curMove;
 
     public int onions;
@@ -23,15 +23,16 @@ public class CustomerMovement : MonoBehaviour
     public int garlic;
     public int hotSauce;
 
-    public bool move;
+    public bool isMove;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = startPos.position;
+        transform.position = startPos.transform.position;
         transform.rotation = Quaternion.Euler(0f, -90f, 0f);
-        moves = new Transform[5];
+
+        moves = new GameObject[5];
 
         moves[0] = startPos;
         moves[1] = orderPos;
@@ -39,7 +40,7 @@ public class CustomerMovement : MonoBehaviour
         moves[3] = pickUpPos;
         moves[4] = leavePos;
 
-        move = false;
+        //isMove = false;
        
         curMove = 0;
     }
@@ -47,57 +48,83 @@ public class CustomerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (moves == null)
+        //if they are moving
+        if (isMove==true)
         {
-            return;
-        }
-
-        if (move==true)
-        {
-            if (curMove == 1 || curMove == 3 || curMove == 4)
+            //if in movement 0, 2, or 4
+            if (curMove == 0||curMove==2||curMove==4)
             {
+                //adjust z by negative value
                 transform.position = transform.position + new Vector3(0f,0f,-0.02f);
-                if (curMove == 3 && transform.position == moves[curMove].position)
-                {
-                    // transform.rotation = Quaternion.Euler(0f, -180f, 0f);
-                    Debug.Log("rotate");
-                }
             }
-            else if (curMove == 2)
+            //if in movement 1
+            if (curMove == 1)
             {
-                transform.position = transform.position + new Vector3(-0.02f, 0f, -0f);
+                //adjust x by pos value
+                transform.position = transform.position + new Vector3(0.02f, 0f, 0f);
             }
+
         }
         
     }
 
     public void nextMove()
     {
-        if (curMove == 1)
+        //if it moved to the counter
+        if (curMove == 0)
         {
-            //transform.rotation = Quaternion.Euler(0f, -180f, 0f);
-            Debug.Log("rotate here");
+            //turns away from counter
+            transform.rotation = Quaternion.Euler(0f, -180f, 0f);
+            //starts moving again
+            isMove = true;
         }
-        else if (curMove == 2 || curMove == 4)
+        else if (curMove == 1||curMove==3)
         {
-            //transform.rotation = Quaternion.Euler(0f, -90f, 0f);
-            Debug.Log("rotate once more");
+            //turns away from counter
+            transform.rotation = Quaternion.Euler(0f, -90f, 0f);
+            //starts moving again
+            isMove = true;
         }
+        else if (curMove == 2)
+        {
+            //turns away from counter
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+            //does not move until it gets the order so isMove is still false
+        }
+        //if it is at the end
+        else if (curMove == 4)
+        {
+            //reset all the positions
+            for(int i =0; i < moves.Length; i++)
+            {
+                moves[i].SetActive(true);
+            }
+            //deactivate
+            gameObject.SetActive(false);
+
+        }
+        //increase current move counter
         curMove++;
     }
 
     void OnTriggerEnter(Collider collider)
     {
-        Debug.Log("enter");
-        if (move = false)
+        if (isMove == false)
         {
             return;
         }
 
+        //if it hits a locator
         if (collider.gameObject.tag == "locator")
         {
-            move = false;
-            Invoke("nextMove()", 2);
+            //deactivate locator so it can move again
+            collider.gameObject.SetActive(false); 
+
+            //turn off movement
+            isMove = false;
+
+            Invoke("nextMove", 1);
+
         }
     }
 }
