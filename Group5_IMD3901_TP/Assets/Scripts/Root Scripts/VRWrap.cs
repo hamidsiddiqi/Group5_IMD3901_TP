053f758d1313938599da1b2b5858f66dffd98d6e
spacejamsr <rootyu14@gmail.com>
2026-03-30 21:28:50 -0400

    updated wrap to be able to be grabbed in vr

---
+++ Group5_IMD3901_TP/Assets/Scripts/Root Scripts/VRWrap.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class VRWrap : MonoBehaviour
+{
+    public float grillDistance = 5f;
+    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable grabInteractable;
+    private bool isGrabbed = false;
+
+    void Start()
+    {
+        grabInteractable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>();
+        grabInteractable.selectEntered.AddListener(OnGrab);
+        grabInteractable.selectExited.AddListener(OnRelease);
+    }
+
+    void OnGrab(SelectEnterEventArgs args)
+    {
+        isGrabbed = true;
+    }
+
+    void OnRelease(SelectExitEventArgs args)
+    {
+        isGrabbed = false;
+    }
+
+    void Update()
+    {
+        if (!isGrabbed) return;
+
+        // press 2 to place wrap on grill
+        if (Keyboard.current.digit2Key.wasPressedThisFrame)
+        {
+            PaniniGrill grill = FindObjectsByType<PaniniGrill>(FindObjectsSortMode.None)[0];
+            if (grill != null && !grill.isCooking)
+            {
+                float dist = Vector3.Distance(transform.position, grill.transform.position);
+                Debug.Log("Distance to grill: " + dist);
+
+                if (dist <= grillDistance)
+                {
+                    // release from hand
+                    grabInteractable.interactionManager.CancelInteractableSelection((UnityEngine.XR.Interaction.Toolkit.Interactables.IXRSelectInteractable)grabInteractable);
+
+                    // snap to grill center
+                    transform.position = new Vector3(
+                        grill.bottomPlate.position.x,
+                        grill.bottomPlate.position.y + 0.1f,
+                        grill.bottomPlate.position.z
+                    );
+                    transform.rotation = Quaternion.Euler(0.014f, 181.611f, -90.514f);
+                    GetComponent<Rigidbody>().isKinematic = true;
+                    grill.currentWrap = gameObject;
+                    Debug.Log("VR: Wrap placed on grill!");
+                }
+            }
+        }
+    }
+}
