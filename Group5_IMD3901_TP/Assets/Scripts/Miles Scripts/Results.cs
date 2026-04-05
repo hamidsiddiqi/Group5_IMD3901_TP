d773fa573b805a509aa8763c398f1d4afcb30cb9
MilesHuschilt <124914551+MilesHuschilt@users.noreply.github.com>
2026-04-04 23:34:06 -0400

    timer and new knife

--- Group5_IMD3901_TP/Assets/Scripts/Miles Scripts/Results.cs
+++ Group5_IMD3901_TP/Assets/Scripts/Miles Scripts/Results.cs
@@ -18,7 +18,11 @@ public class Results : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 
     // Update is called once per frame
--- Group5_IMD3901_TP/Assets/Scripts/Miles Scripts/TImer.cs
+++ Group5_IMD3901_TP/Assets/Scripts/Miles Scripts/TImer.cs
@@ -39,6 +39,9 @@ public class TImer : MonoBehaviour
     void Start()
     {
         timerText.SetText(time.ToString());
+
+        StartTimer(time);
+
     }
 
     // Update is called once per frame
