49f4c7b148b8491e14923fdcf4b8dab8b665a7c7
MilesHuschilt <124914551+MilesHuschilt@users.noreply.github.com>
2026-04-04 20:02:01 -0400

    result adjustments

--- Group5_IMD3901_TP/Assets/Scripts/Miles Scripts/Results.cs
+++ Group5_IMD3901_TP/Assets/Scripts/Miles Scripts/Results.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Results : MonoBehaviour
 {
@@ -9,6 +11,10 @@ public class Results : MonoBehaviour
 
     public TextMeshProUGUI customerText;
     public TextMeshProUGUI MoneyText;
+
+    public AudioSource hoverSound;
+    public AudioSource press;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,4 +28,59 @@ public class Results : MonoBehaviour
         customerText.SetText(CustomersServed.ToString());
         MoneyText.SetText(Money.ToString());
     }
+
+    public void GoToTitle()
+    {
+        press.Play();
+        SceneManager.LoadScene("TitleScreen");
+    }
+
+    public void grow(TextMeshProUGUI hoverBut)
+    {
+        hoverSound.Play();
+        hoverBut.fontSize = 24;
+
+        if (hoverBut.fontSize < 30)
+        {
+            StartCoroutine(grow2(hoverBut));
+        }
+
+    }
+
+    public void shrink(TextMeshProUGUI hoverBut)
+    {
+        hoverBut.fontSize = 29;
+
+        if (hoverBut.fontSize > 20)
+        {
+            StartCoroutine(shrink2(hoverBut));
+        }
+
+    }
+
+    public IEnumerator grow2(TextMeshProUGUI hoverBut)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+
+            hoverBut.fontSize += 1;
+            hoverBut.color = Color.Lerp(hoverBut.color, Color.gray2, 0.5f);
+            yield return new WaitForSeconds(0.025f);
+        }
+
+    }
+
+    public IEnumerator shrink2(TextMeshProUGUI hoverBut)
+    {
+        // playButText.fontSize = 24;
+
+        for (int i = 0; i < 5; i++)
+        {
+
+            hoverBut.fontSize -= 1;
+            hoverBut.color = Color.Lerp(hoverBut.color, Color.black, 0.5f);
+            yield return new WaitForSeconds(0.025f);
+        }
+    }
+
 }
